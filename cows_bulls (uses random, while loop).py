db6bc363d16079e766197404583bdb502521cf76
vengrinovich <p.vengrinovich@gmail.com>
2017-12-20 20:26:50 -0800

    description edit

--- cows_bulls (uses random, while loop).py
+++ cows_bulls (uses random, while loop).py
@@ -1,3 +1,12 @@
+"""Create a program that will play the “cows and bulls” game with the user. The game works like this:
+
+Randomly generate a 4-digit number. Ask the user to guess a 4-digit number. 
+For every digit that the user guessed correctly in the correct place, they have a “cow”. 
+For every digit the user guessed correctly in the wrong place is a “bull.” 
+Every time the user makes a guess, tell them how many “cows” and “bulls” they have. 
+Once the user guesses the correct number, the game is over.
+source - http://www.practicepython.org/exercise/2014/07/05/18-cows-and-bulls.html"""
+
 import random
 
 def cows_bulls():
