cbd9441f58ad877d8a02d837985cb1f42db33ae3
vengrinovich <p.vengrinovich@gmail.com>
2017-12-23 17:04:27 -0800

    Checks if the number provided is a prime

--- is_this_number_prime.py
+++ is_this_number_prime.py
@@ -1,5 +1,5 @@
 """Ask the user for a number and determine whether the number is prime or not. 
-(For those who have forgotten, a prime number is a number that has no divisors.)"""
+(For those who have forgotten, a prime number is a number that has no divisors.) """
 
 def prime_or_not():
 	number = int(raw_input("Give me a number please:"))
