cb79b06da5bb8d3737198cdb3440f341a54e62d2
vengrinovich <p.vengrinovich@gmail.com>
2017-12-23 17:25:46 -0800

    Main CSV manipulation reference

--- practice_csv.py
+++ practice_csv.py
@@ -1,13 +1,15 @@
 # always use the csv module for reading and writing CSV files
 # this is the CSV file used in this example program:
 
-"""4/5/2015 13:34,Apples,73
+"""
+4/5/2015 13:34,Apples,73
 4/5/2015 3:41,Cherries,85
 4/6/2015 12:46,Pears,14
 4/8/2015 8:59,Oranges,52
 4/10/2015 2:07,Apples,152
 4/10/2015 18:10,Bananas,23
-4/10/2015 2:40,Strawberries,98"""
+4/10/2015 2:40,Strawberries,98
+"""
 
 import csv 
 
