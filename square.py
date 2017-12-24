c3cc25e2b2f264ef730a1f56968a8f733222e9bc
vengrinovich <p.vengrinovich@gmail.com>
2017-12-23 17:15:26 -0800

    Squares a given value, re-usable program example

--- square.py
+++ square.py
@@ -1,8 +1,11 @@
+# example of how you can now use import square and use this function
+# from another program
+
 def square(num):
     return num * num
     
 
 if __name__ == "__main__":
 	num = input("Give me a number: ")
-	print "Hello from: %s " % __name__
+	print "Hello from: %s "  % __name__
  	print(square(num))
