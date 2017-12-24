d2e267a458c5148dd33a399295f1249b7159b607
vengrinovich <p.vengrinovich@gmail.com>
2017-12-23 17:11:56 -0800

    Uses ascii from string module

--- random_pw_generator.py
+++ random_pw_generator.py
@@ -5,7 +5,7 @@ for a new password. Include your run-time code in a main method.
 
 Extra:
 Ask the user how strong they want their password to be. For weak passwords, 
-pick a word or two from a list."""
+pick a word or two from a list. """
 
 import random, string 
 
